7d55a653dba9545c21e1d31f1bc3f2c99723ec1d
Thiago Gomes <thiagogomesabreu01@gmail.com>
2025-10-31 00:55:39 -0300

    feat(settings): adicionadas configurações para autenticação
    Definição da classe genérica user como modelo de autenticação padrão do
    django, além disso foram feitas modificações em relação a api definindo
    a forma padrão de autenticação com token simplificado e uma forma padrão
    de verificar se o usuário está autenticado.

--- core/settings.py
+++ core/settings.py
@@ -38,9 +38,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
     'rest_framework',
     'api',
 ]
+
+AUTH_USER_MODEL = 'api.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
