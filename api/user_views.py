5791378bfd8010e8f4b65ba0340d064e1345cf77
Thiago Gomes <thiagogomesabreu01@gmail.com>
2025-10-31 00:50:07 -0300

    feat(user_views): novas views para usuário
    Adicionadas novas views para criação e edição de usuário, para realizar
    login criando o token, verificação de autenticação com a view profile e uma view para
    logout do usuário apagando o token. Além disso, leve refatoração nas
    outras views tirando a função de criptografar a senha ao atualizar um usuário das mesmas,
    função agora presente no serializer do usuário.

---
+++ api/user_views.py
@@ -0,0 +1,104 @@
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authtoken.models import Token
+from .models import User, Researcher, Collaborator, Company
+from .serializers import (
+    ResearcherProfileSerializer,
+    CollaboratorProfileSerializer,
+    CompanyProfileSerializer,
+    LoginSerializer,
+    UserSerializer
+)
+
+
+class ResearcherListCreateView(generics.ListCreateAPIView):
+    permission_classes = [AllowAny]
+    queryset = Researcher.objects.all()
+    serializer_class = ResearcherProfileSerializer
+
+class ResearcherDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [AllowAny]
+    queryset = Researcher.objects.all()
+    serializer_class = ResearcherProfileSerializer
+
+
+
+class CollaboratorListCreateView(generics.ListCreateAPIView):
+    permission_classes = [AllowAny]
+    queryset = Collaborator.objects.all()
+    serializer_class = CollaboratorProfileSerializer
+
+class CollaboratorDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [AllowAny]
+    queryset = Collaborator.objects.all()
+    serializer_class = CollaboratorProfileSerializer
+
+
+
+class CompanyListCreateView(generics.ListCreateAPIView):
+    permission_classes = [AllowAny]
+    queryset = Company.objects.all()
+    serializer_class = CompanyProfileSerializer
+
+class CompanyDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [AllowAny]
+    queryset = Company.objects.all()
+    serializer_class = CompanyProfileSerializer
+    
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data['user']
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response({
+            "token": token.key,
+            "user": UserSerializer(user).data
+        })
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "email": user.email,
+            "user_type": user.user_type,
+            "is_authenticated": True
+        })
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+            try:
+                token = Token.objects.get(user=request.user)
+                token.delete()
+            except Token.DoesNotExist:
+                return Response({"detail": "Token não encontrado."}, status=status.HTTP_400_BAD_REQUEST)
+
+            return Response({"detail": "Logout realizado com sucesso."}, status=status.HTTP_200_OK)
