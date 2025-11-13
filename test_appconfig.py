f4beaae77bd9811d63a584501250040404ddd1df
Gabriel Robson <122059940+Gabrielxcx@users.noreply.github.com>
2025-11-13 16:29:37 -0300

    adicionando teste do app.py

--- test_appconfig.py
+++ test_appconfig.py
@@ -2,15 +2,12 @@ import os
 import sys
 from importlib import import_module
 
-# Garante que o diretório atual está no sys.path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 try:
-    # Importa a classe normalmente
     api_apps = import_module("api.apps")
     ApiConfig = getattr(api_apps, "ApiConfig")
 
-    # Cria uma subclasse temporária com o 'path' definido (sem alterar o código original)
     class TestApiConfig(ApiConfig):
         path = os.path.dirname(os.path.abspath(api_apps.__file__))
 
